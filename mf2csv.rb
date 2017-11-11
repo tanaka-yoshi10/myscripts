4ee9b42fae42312f4527bab8267756121db71fd0
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2017-11-11 14:15:03 +0900

    indentなど調整

--- mf2csv.rb
+++ mf2csv.rb
@@ -4,15 +4,14 @@ def usage
 	exit
 end
 
-
 # Main process
 usage if ARGV.size < 1
 infilename = ARGV[0]
 exit unless FileTest.exist?( infilename )
 outfilename = File.dirname(infilename) + "\\" + File.basename(infilename, ".*") + "_rslt.csv"
 h_files = {
-  'IN'  => open(  infilename, "r" ),
-  'OUT' => open( outfilename, "w" )
+	'IN'  => open(  infilename, "r" ),
+	'OUT' => open( outfilename, "w" )
 }
 
 # line 1
@@ -20,9 +19,8 @@ line =  h_files['IN'].gets
 h_files['OUT'].puts "date,content,amount,account,major_category,minor_category,memo,check,HT,M,Y,W,HT,M,Y,W"
 
 while line =  h_files['IN'].gets
-
-  line.chomp! unless line==nil
-  a_word = line.split("\"\,\"")
+	line.chomp! unless line==nil
+	a_word = line.split("\"\,\"")
 
 	next if a_word[0]=="\"0"    # don't calculate
 	next if a_word[3].to_i > 0  # income
@@ -31,41 +29,39 @@ while line =  h_files['IN'].gets
 	for i in 1..7 ; output << a_word[i] ; end
 	output << "" # output[7] for check
 
-  # Rules for Investiment
+	# Rules for Investiment
 	case a_word[4] # account
-	when /^ht_/ ; output += [1.0, 0.0, 0.0, 0.0]
-  when /^m_/  ; output += [0.0, 1.0, 0.0, 0.0]
-  when /^y_/  ; output += [0.0, 0.0, 1.0, 0.0]
-  when /^w_/  ; output += [0.0, 0.0, 0.0, 1.0]
-  else
-		output += [0.0,0.0,0.0,0.0] ; output[7] << "unknown_account;"
-  end
+		when /^ht_/ ; output += [1.0, 0.0, 0.0, 0.0]
+		when /^m_/  ; output += [0.0, 1.0, 0.0, 0.0]
+		when /^y_/  ; output += [0.0, 0.0, 1.0, 0.0]
+		when /^w_/  ; output += [0.0, 0.0, 0.0, 1.0]
+		else
+			output += [0.0,0.0,0.0,0.0] ; output[7] << "unknown_account;"
+	end
 
-  # Rules for Expenses
+	# Rules for Expenses
 	major = a_word[5].encode("UTF-8") ; minor = a_word[6].encode("UTF-8")
 	content = a_word[2].encode("UTF-8")
 
-  if minor[0] != "["
+	if minor[0] != "["
 		output += [0.0,0.0,0.0,0.0] ; output[7] << "unknown_category;"
 	elsif major == "通信費" ; output += [0.25, 0.0, 0.5, 0.25]
 	elsif content == "FeBe"; output += [0.0, 0.0, 1.0, 0.0]
 	elsif content =~ /ブリアン/ ; output += [0.0, 0.0, 1.0, 0.0]
 	elsif minor =~ /NT\]$/ ; output += [0.25, 0.0, 0.5, 0.25]
 	elsif minor =~ /MY\]$/ ; output += [0.0, 0.0, 1.0, 0.0]
-  elsif minor =~ /MW\]$/ ; output += [0.0, 0.5, 0.0, 0.5]
+	elsif minor =~ /MW\]$/ ; output += [0.0, 0.5, 0.0, 0.5]
 	elsif minor =~ /HT\]$/ ; output += [1.0, 0.0, 0.0, 0.0]
 	elsif minor =~ /M\]$/  ; output += [0.0, 1.0, 0.0, 0.0]
 	elsif minor =~ /Y\]$/  ; output += [0.0, 0.0, 1.0, 0.0]
 	elsif minor =~ /W\]$/  ; output += [0.0, 0.0, 0.0, 1.0]
 	else
 		output += [0.0,0.0,0.0,0.0] ; output[7] << "need to input;"
-  end
+	end
 
 	output.each { |a| ; h_files['OUT'].print "\"#{a}\"," }
-  h_files['OUT'].puts
-
+	h_files['OUT'].puts
 end
-
 
 ### close output files
 h_files.each_value { |f| ; unless f.closed? ; f.close ; end }
