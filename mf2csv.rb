35e6aefd22504cd066f5d24c00df44d54fb81249
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2017-11-11 17:10:33 +0900

    リファクタリング

--- mf2csv.rb
+++ mf2csv.rb
@@ -15,46 +15,61 @@ outfilename = File.expand_path("../#{File.basename(infilename, '.*')}_rslt.csv",
 
 File.open(outfilename, 'w' ) do |out_file|
   out_file.puts 'date,content,amount,account,major_category,minor_category,memo,check,HT,M,Y,W,HT,M,Y,W'
-  CSV.foreach(infilename, headers: :first_row, encoding: 'Shift_JIS:UTF-8') do |a_word|
-    next if a_word['計算対象'] == '0'
-    next if a_word['金額（円）'].to_i > 0
+  CSV.foreach(infilename, headers: :first_row, encoding: 'Shift_JIS:UTF-8') do |input_row|
+    next if input_row['計算対象'] == '0'
+    next if input_row['金額（円）'].to_i > 0 # 収入は見ない
 
-    output = []
-    for i in 1..7 ; output << a_word[i] ; end
+    output = input_row[1..7]
     output << '' # output[7] for check
 
     # Rules for Investiment
-    case a_word['保有金融機関']
-      when /^ht_/ ; output += [1.0, 0.0, 0.0, 0.0]
-      when /^m_/  ; output += [0.0, 1.0, 0.0, 0.0]
-      when /^y_/  ; output += [0.0, 0.0, 1.0, 0.0]
-      when /^w_/  ; output += [0.0, 0.0, 0.0, 1.0]
+    case input_row['保有金融機関']
+      when /^ht_/
+        output += [1.0, 0.0, 0.0, 0.0]
+      when /^m_/
+        output += [0.0, 1.0, 0.0, 0.0]
+      when /^y_/
+        output += [0.0, 0.0, 1.0, 0.0]
+      when /^w_/
+        output += [0.0, 0.0, 0.0, 1.0]
       else
-        output += [0.0,0.0,0.0,0.0] ; output[7] << 'unknown_account;'
+        output += [0.0,0.0,0.0,0.0]
+        output[7] << 'unknown_account;'
     end
 
     # Rules for Expenses
-    major = a_word['大項目'].encode('UTF-8')
-    minor = a_word['中項目'].encode('UTF-8')
-    content = a_word['内容'].encode('UTF-8')
+    major = input_row['大項目'].encode('UTF-8')
+    minor = input_row['中項目'].encode('UTF-8')
+    content = input_row['内容'].encode('UTF-8')
 
-    if minor[0] != '['
-      output += [0.0,0.0,0.0,0.0] ; output[7] << 'unknown_category;'
-    elsif major == '通信費' ; output += [0.25, 0.0, 0.5, 0.25]
-    elsif content == 'FeBe'; output += [0.0, 0.0, 1.0, 0.0]
-    elsif content =~ /ブリアン/ ; output += [0.0, 0.0, 1.0, 0.0]
-    elsif minor =~ /NT\]$/ ; output += [0.25, 0.0, 0.5, 0.25]
-    elsif minor =~ /MY\]$/ ; output += [0.0, 0.0, 1.0, 0.0]
-    elsif minor =~ /MW\]$/ ; output += [0.0, 0.5, 0.0, 0.5]
-    elsif minor =~ /HT\]$/ ; output += [1.0, 0.0, 0.0, 0.0]
-    elsif minor =~ /M\]$/  ; output += [0.0, 1.0, 0.0, 0.0]
-    elsif minor =~ /Y\]$/  ; output += [0.0, 0.0, 1.0, 0.0]
-    elsif minor =~ /W\]$/  ; output += [0.0, 0.0, 0.0, 1.0]
+    if minor[0] != '[' # MoneyForwardで自動で振り分けられた場合
+      output += [0.0,0.0,0.0,0.0]
+      output[7] << 'unknown_category;'
+    elsif major == '通信費'
+      output += [0.25, 0.0, 0.5, 0.25]
+    elsif content == 'FeBe'
+      output += [0.0, 0.0, 1.0, 0.0]
+    elsif content =~ /ブリアン/
+      output += [0.0, 0.0, 1.0, 0.0]
+    elsif minor =~ /NT\]$/
+      output += [0.25, 0.0, 0.5, 0.25]
+    elsif minor =~ /MY\]$/
+      output += [0.0, 0.0, 1.0, 0.0]
+    elsif minor =~ /MW\]$/
+      output += [0.0, 0.5, 0.0, 0.5]
+    elsif minor =~ /HT\]$/
+      output += [1.0, 0.0, 0.0, 0.0]
+    elsif minor =~ /M\]$/
+      output += [0.0, 1.0, 0.0, 0.0]
+    elsif minor =~ /Y\]$/
+      output += [0.0, 0.0, 1.0, 0.0]
+    elsif minor =~ /W\]$/
+      output += [0.0, 0.0, 0.0, 1.0]
     else
-      output += [0.0,0.0,0.0,0.0] ; output[7] << 'need to input;'
+      output += [0.0,0.0,0.0,0.0]
+      output[7] << 'need to input;'
     end
 
-    output.each { |a| out_file.print "\"#{a}\"," }
-    out_file.puts
+    out_file.puts output.map { |e| %Q!"#{e}"! }.join(',')
   end
 end
